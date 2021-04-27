5162a4c36c410e76a7a0e210de768e74363c4bdb
nk8713694 <nk8713694@gmail.com>
2021-04-27 07:22:35 +0530

    displayed products dynamically

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike slim shirts',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'a high quality product'
+        },
+        {
+            _id:'2',
+            name:'adidas slim shirts',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'a high quality product'
+        },
+        {
+            _id:'3',
+            name:' boomba freee slim shirts',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.8,
+            numReviews:17,
+            description:'a high quality product'
+        }
+        ,
+        {
+            _id:'4',
+            name:'Nike slim pants',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:158,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'a high quality product'
+        }
+        ,
+        {
+            _id:'5',
+            name:'Nike slim shirts',
+            category:'Pants',
+            image:'/images/p5.jpg',
+            price:60,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'a high quality product'
+        }
+        ,
+        {
+            _id:'6',
+            name:'adids slim shirts',
+            category:'Shirts',
+            image:'/images/p6.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:15,
+            description:'a high quality product'
+        }
+    ]
+}
+
+
+export default data;
