2eaa3ab55829c229a9767a331c3a00f96e4063e0
nk8713694 <nk8713694@gmail.com>
2021-05-03 13:51:34 +0530

    product model and product router created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-
+/* eslint-disable no-unused-vars */
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
@@ -11,43 +11,17 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useCreateIndex: true,
 });
 
-
-
-
-app.get('/api/products/:id',(req,res)=>{
-    const product=data.products.find((x)=>x._id==req.params.id);
-    if(product)
-    {
-        res.send(product);
-    }
-    else{
-        res.status(404).send({message:'prdodcut not found'})
-    }
-})
-
-
-
-app.get('/api/products',(req,res)=>{
-    res.send(data.products)
-})
-
-
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
-app.get('/',(req,res)=>{
-    res.send("server is up and running")
-})
-
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res,next) => {
-    res.status(500).send({ message: err.message });
-  });
-  
-
-
-const port =process.env.PORT||5000
-app.listen(port,()=>{
-    console.log(`served at http://localhost:${port}`);
-    
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
