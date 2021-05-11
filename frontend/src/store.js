7e4a59a8c20b0b3444cd94afe6883fbbd28b5d3c
nk8713694 <nk8713694@gmail.com>
2021-05-11 09:58:15 +0530

    shipping page created and implemented

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,14 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
-import { userSigninReducer, userRegisterReducer } from './reducers/userReducers';
 import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
+import {
+  userRegisterReducer,
+  userSigninReducer,
+} from './reducers/userReducers';
 
 const initialState = {
   userSignin: {
@@ -17,6 +20,9 @@ const initialState = {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : {},
   },
 };
 const reducer = combineReducers({
